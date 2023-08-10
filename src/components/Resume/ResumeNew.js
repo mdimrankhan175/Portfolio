5da1e724cf113a00b654de6aed69c7776f53959e
SomeshBasak <someshbasak2016@gmail.com>
2023-08-10 12:45:02 +0530

    Updated New Resume .js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Somesh_Basak_IT-CV.pdf";
+import pdf from "../../Assets/../Assets/Somesh_Basak IT-CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
