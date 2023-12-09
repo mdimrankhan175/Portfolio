faa402f02accfddca8d4bb079301e78596294759
MOHAMMAD IMRAN KHAN <124260776+mdimrankhan175@users.noreply.github.com>
2023-12-09 07:13:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Somesh Basak</h3>
+          <h3>Developed by Imran Khan</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SB</h3>
